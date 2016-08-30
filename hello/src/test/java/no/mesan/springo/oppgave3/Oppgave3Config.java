05342950ee3335e255b925b1e164d2e0ed749f8f
Torbjørn Skyberg Knutsen <torbjornk@mesan.no>
2016-08-30 17:30:43 +0200

    Riktig component scan..

--- hello/src/test/java/no/mesan/springo/oppgave3/Oppgave3Config.java
+++ hello/src/test/java/no/mesan/springo/oppgave3/Oppgave3Config.java
@@ -12,7 +12,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  * @author Torbjørn S. Knutsen
  */
 @Configuration
-@ComponentScan(basePackages = "no.mesan.spring.oppgave3")
+@ComponentScan(basePackages = "no.mesan.springo.oppgave3")
 @PropertySource("classpath:spring.properties")
 public class Oppgave3Config {
     @Bean
